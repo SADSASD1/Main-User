cd27fdc66cfef9e00eeaa98b70813b931e9663d6
SADSASD1 <aleqsanyan1996@.ru>
2018-11-13 16:27:40 +0400

    Main User

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        User us=new User();
+        System.out.println(us.firstname);
+        System.out.println(us.lastname);
+        System.out.println(us.email);
+        System.out.println(us.password);
+    }
+}

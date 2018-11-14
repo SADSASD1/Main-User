048a536424b20ebba3425bf3ba7356094ae4b643
SADSASD1 <42295051+SADSASD1@users.noreply.github.com>
2018-11-14 17:23:53 +0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        User us=new User();
-        System.out.println(us.firstname);
-        System.out.println(us.lastname);
-        System.out.println(us.email);
-        System.out.println(us.password);
-    }
-}

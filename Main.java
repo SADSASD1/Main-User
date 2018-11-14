0e6321a518708229a1ccf47a23788a011c99eaea
SADSASD1 <aleqsanyan1996@.ru>
2018-11-14 17:31:12 +0400

    Changed Main-User

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        User us = new User();
+        System.out.println(us.getFirstname());
+        System.out.println(us.getLastname());
+        System.out.println(us.getEmail());
+        System.out.println(us.getPassword());
+    }
+}
---
+++ User.java
@@ -0,0 +1,50 @@
+public class User extends Main  {
+    private String firstname = new String("Enter your Firstname");
+    private String lastname = new String("Enter your Lasttname");
+    private String email = new String("Enter your Email");
+    private String password = new String("Enter your Password");
+
+    public User() {
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String firstname, String lastname, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+
+    }
+}

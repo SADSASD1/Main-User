d024f3282ffc0cfcf5245f9fbcb4bb727bb2b85d
SADSASD1 <42295051+SADSASD1@users.noreply.github.com>
2018-11-14 17:24:10 +0400

    Delete User.java

--- User.java
+++
@@ -1,9 +0,0 @@
-
-public  class User extends Main {
-    public String firstname=new String("Enter your Firstname");
-    public String lastname= new String("Enter your Lastname");
-    public String email=new String("Enter your Email");
-    public String password=new String("Enter your Password");
-
-
-}
